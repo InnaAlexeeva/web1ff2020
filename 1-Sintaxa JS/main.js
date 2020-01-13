dabe78db12e9a546adb6bc98e258c3f72a80caf5
usarb <chilatsergiu@gmail.com>
2020-01-13 11:02:16 +0200

    1. Instructiuni repetitive

--- 1-Sintaxa JS/main.js
+++ 1-Sintaxa JS/main.js
@@ -36,5 +36,18 @@ switch(cifra){
 }
 
 
+for(let i = 1; i <=5; i++){
+	console.log(i);	
+}
 
+let i = 1;
+while( i <=5){
+	console.log(i);	
+	i++;
+}
 
+i = 1;
+do{
+	console.log(i);	
+	i++;
+}while( i <=5);
