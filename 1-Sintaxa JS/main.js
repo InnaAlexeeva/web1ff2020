9ea74e5c37d18c5f3743a5439a247a52065fa3b5
usarb <chilatsergiu@gmail.com>
2020-01-13 10:58:29 +0200

    1 - Sintaxa JS

---
+++ 1-Sintaxa JS/main.js
@@ -0,0 +1,40 @@
+//alert("Hello world");
+
+let a = 3;
+let b = 5;
+
+//console.log(a + b)
+console.log(suma(3, 3));
+
+function suma(a, b){
+	return a + b;
+}
+
+const n = 2;
+if(n > 0){
+	console.log("Pozitiv")
+} else if(n < 0) {
+	console.log("Negativ")
+} else {
+	console.log("Null")
+}
+
+
+let cifra = 2;
+switch(cifra){
+	case 1: 
+		console.log("Unu")
+		break;
+	case 2: 
+		console.log("Doi")
+		break;
+	case 3: 
+		console.log("Trei")
+		break;
+	default: 
+		console.log("Nu est ecifra")
+}
+
+
+
+
