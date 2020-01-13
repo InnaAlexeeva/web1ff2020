3c4bd1e2e79594f6d31a31f22de26043a37c5a55
usarb <chilatsergiu@gmail.com>
2020-01-13 11:08:46 +0200

    1. Siruri de caractere

--- 1-Sintaxa JS/main.js
+++ 1-Sintaxa JS/main.js
@@ -6,9 +6,13 @@ let b = 5;
 //console.log(a + b)
 console.log(suma(3, 3));
 
+
+/*Functie JS*/
 function suma(a, b){
 	return a + b;
 }
+
+/*Instructiuni de decizie*/
 
 const n = 2;
 if(n > 0){
@@ -34,7 +38,7 @@ switch(cifra){
 	default: 
 		console.log("Nu est ecifra")
 }
-
+/*Instructiuni repetitive*/
 
 for(let i = 1; i <=5; i++){
 	console.log(i);	
@@ -51,3 +55,14 @@ do{
 	console.log(i);	
 	i++;
 }while( i <=5);
+
+
+/*Siruri de caractere*/
+console.clear();
+
+const name = "Ion Creanga";
+
+console.log(name);
+console.log(name.length);
+console.log(name.split(" "));
+
