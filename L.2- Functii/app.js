c810279e6f66085559a0c4938e21c8754b47ad26
Sergiu Chilat <chilatsergiu@gmail.com>
2020-05-05 15:44:24 +0300

    Utilizarea functiilor (p2)

--- L.2- Functii/app.js
+++ L.2- Functii/app.js
@@ -1,9 +1,18 @@
 document.addEventListener("DOMContentLoaded", function(){
-  document.body.style.backgroundColor = 'red';
-  changeBackgroundColor('green')
-  changeBackgroundColor('yellow')
-  
+  init();
 });
+
+function init(){
+  const colors = ["red", "green", "blue", "cyan", "pink"];
+  
+  for(const color of colors){
+    const button = document.createElement("button");
+    button.innerText = color;
+    button.id = `${color}_button`;
+    document.body.appendChild(button);
+    changeBackgroundColor(color);
+  }
+}
 
 function changeBackgroundColor(color){
   document.getElementById(`${color}_button`).addEventListener("click", function () {
